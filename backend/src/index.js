bf7ffc6a6f9122060e0f2119dfe1ae5e67fa0fac
mujahid1243 <muslim25351@gmail.com>
2025-05-10 13:39:14 +0300

    installing frontend dependency and setting up page route

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,10 @@ import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+
+
 dotenv.config();
+
 const port = process.env.PORT
 const app = express();
 app.use(express.json());
