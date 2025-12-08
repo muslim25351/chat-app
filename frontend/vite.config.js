de9167344ca5ea96db940e7b3041294951f95a76
muslim25351 <muslim25351@gmail.com>
2025-12-09 00:44:28 +0300

    build chunk size

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss("./tailwind.config.js")],
+  build: {
+    chunkSizeWarningLimit: 1500, // size in kB (default is 500)
+  },
 });
